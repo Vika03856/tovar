5f60568819bc56c5f11facce43daddbb7f9c5b0e
Vika03856 <viktoriya03856@yandex.by>
2024-03-15 20:42:52 +0300

    DOCTYPE html

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
-
+from flask import render_template
 @app.route('/')
-def hello_world():
-    return 'Привет друг. Меня зовут ВИка!'
+@app.route('/index')
+def index():
+    # goods = [{"id":"1", "name":"Папка", "q":"20"}, {"id":"2", "name":"карандаш", "q":"10"}]
+    goods = [{'name': 'Blok', 'q': 30, 'id': 1}, {'name': 'Paper Navikator', 'q': 188, 'id': 2},
+     {'name': 'Diary', 'q': 15, 'id': 3}]
+    page = render_template("table.html", goods2 = goods)
+    return page
 
 @app.route('/kontaks')
 def kontaks():
